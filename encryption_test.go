dd55d50a873aeb8a4bcf5edd29160cfce0dd57bf
Tarith <tarithj@gmail.com>
2020-12-10 12:37:57 +0530

    test: updated encryption/decryption benchmark

--- encryption_test.go
+++ encryption_test.go
@@ -74,24 +74,26 @@ func BenchmarkEncryptDecrypt(t *testing.B) {
 		{"filecrypt", Passphrase("[][]-fgioamfifo;hkaf")},
 		{"95k20kd-2j4", Passphrase("[]'fd;fo;hkaf")},
 	}
-	for k, test := range tests {
-		testCase := test
-		t.Run(fmt.Sprint(k), func(t *testing.B) {
-			encryptedResults, err := EncryptSHA256([]byte(testCase.input), testCase.passphrase)
-			if err != nil {
-				t.Errorf("unexpected error: %s", err)
-				return
-			}
+	for i := 0; i < 10; i++ {
+		for k, test := range tests {
+			testCase := test
+			t.Run(fmt.Sprint(k), func(t *testing.B) {
+				encryptedResults, err := EncryptSHA256([]byte(testCase.input), testCase.passphrase)
+				if err != nil {
+					t.Errorf("unexpected error: %s", err)
+					return
+				}
 
-			decryptedResults, err := DecryptSHA256(encryptedResults, testCase.passphrase)
-			if err != nil {
-				t.Errorf("unexpected error: %s", err)
-				return
-			}
+				decryptedResults, err := DecryptSHA256(encryptedResults, testCase.passphrase)
+				if err != nil {
+					t.Errorf("unexpected error: %s", err)
+					return
+				}
 
-			if exp, got := string(decryptedResults), testCase.input; exp != got {
-				t.Errorf("expected result %s, got %s", exp, got)
-			}
-		})
+				if exp, got := string(decryptedResults), testCase.input; exp != got {
+					t.Errorf("expected result %s, got %s", exp, got)
+				}
+			})
+		}
 	}
 }
